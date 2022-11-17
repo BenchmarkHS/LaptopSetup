360fa3e9cc22133f696469f745106708b8b266a5
David Herrmann <dherrmann@benchmarkhs.com>
2022-11-17 10:24:44 -0500

    User creation working. Added changing the Admin password

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,13 +33,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnSetUser = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdminPass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSetAdminPass = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Location = new System.Drawing.Point(14, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(87, 20);
             this.label1.TabIndex = 0;
@@ -48,7 +53,7 @@
             // txtUsername
             // 
             this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsername.Location = new System.Drawing.Point(105, 12);
+            this.txtUsername.Location = new System.Drawing.Point(107, 46);
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(150, 26);
             this.txtUsername.TabIndex = 1;
@@ -57,7 +62,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(261, 18);
+            this.label2.Location = new System.Drawing.Point(263, 52);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 2;
@@ -66,7 +71,7 @@
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPassword.Location = new System.Drawing.Point(349, 12);
+            this.txtPassword.Location = new System.Drawing.Point(351, 46);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(150, 26);
             this.txtPassword.TabIndex = 3;
@@ -74,7 +79,7 @@
             // btnSetUser
             // 
             this.btnSetUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSetUser.Location = new System.Drawing.Point(522, 10);
+            this.btnSetUser.Location = new System.Drawing.Point(507, 44);
             this.btnSetUser.Name = "btnSetUser";
             this.btnSetUser.Size = new System.Drawing.Size(100, 30);
             this.btnSetUser.TabIndex = 4;
@@ -82,11 +87,65 @@
             this.btnSetUser.UseVisualStyleBackColor = true;
             this.btnSetUser.Click += new System.EventHandler(this.btnSetUser_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Create New User";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Set Admin Password";
+            // 
+            // txtAdminPass
+            // 
+            this.txtAdminPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAdminPass.Location = new System.Drawing.Point(107, 131);
+            this.txtAdminPass.Name = "txtAdminPass";
+            this.txtAdminPass.Size = new System.Drawing.Size(150, 26);
+            this.txtAdminPass.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(19, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Password:";
+            // 
+            // btnSetAdminPass
+            // 
+            this.btnSetAdminPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSetAdminPass.Location = new System.Drawing.Point(263, 129);
+            this.btnSetAdminPass.Name = "btnSetAdminPass";
+            this.btnSetAdminPass.Size = new System.Drawing.Size(100, 30);
+            this.btnSetAdminPass.TabIndex = 9;
+            this.btnSetAdminPass.Text = "Set";
+            this.btnSetAdminPass.UseVisualStyleBackColor = true;
+            this.btnSetAdminPass.Click += new System.EventHandler(this.btnSetAdminPass_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSetAdminPass);
+            this.Controls.Add(this.txtAdminPass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnSetUser);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label2);
@@ -106,6 +165,11 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnSetUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdminPass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSetAdminPass;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.DirectoryServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
@@ -24,28 +25,57 @@ namespace LaptopSetup
 
         private void btnSetUser_Click(object sender, EventArgs e)
         {
+            bool success = true;
             try
             {
-                string strCmdText;
-                strCmdText = $"net user {txtUsername.Text} {txtPassword.Text} /add";
-                Process process = new Process();
-                ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe", strCmdText);
-                //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                startInfo.Verb = "runas";
-                process.StartInfo = startInfo;
-                process.Start();
-                //Process.Start("CMD.exe", strCmdText);
+                RunCommand($"/C net user {txtUsername.Text} {txtPassword.Text} /add");
             }
             catch (Exception ex)
             {
+                success = false;
                 MessageBox.Show($"ERROR: {ex}");
             }
             finally
             {
-                MessageBox.Show($"User: {txtUsername.Text} created!");
-                txtPassword.Text = null;
-                txtUsername.Text = null; 
+                if (success)
+                {
+                    MessageBox.Show($"User: {txtUsername.Text} created!");
+                    txtPassword.Text = null;
+                    txtUsername.Text = null;
+                }
             }
+        }
+
+        private void btnSetAdminPass_Click(object sender, EventArgs e)
+        {
+            bool success = true;
+            try
+            {
+                RunCommand($"/C net user Administrator {txtAdminPass.Text}");
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                MessageBox.Show($"ERROR: {ex}");
+            }
+            finally
+            {
+                if (success)
+                {
+                    MessageBox.Show($"Admin Password Set!");
+                    txtPassword.Text = null;
+                    txtUsername.Text = null;
+                }
+            }
+        }
+
+        private void RunCommand(string strCmdText)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe", strCmdText);
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo = startInfo;
+            process.Start();
         }
     }
 }
